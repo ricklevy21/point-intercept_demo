1ea79ffa1c6ef128fb1133513668fbdcb068e990
Rick Levy <Richard.Levy@BotanicGardens.org>
2021-06-09 10:45:36 -0600

    mongoose uri

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // Connect to the Mongo DB
-mongoose.connect("process.env.MONGODB_URI");
+mongoose.connect(process.env.MONGODB_URI);
 //"mongodb://localhost/point-intercept"
 //"process.env.MONGODB_URI"
 
